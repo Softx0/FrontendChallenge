3b6c51c6c1c2bf005d4e8fff7d17b3152afcba34
Emma <ing.valen0421@hotmail.com>
2023-04-17 19:59:42 -0400

    feat: some styles

--- src/pages/CondimentumConsectetur/CondimentumConsectetur.tsx
+++ src/pages/CondimentumConsectetur/CondimentumConsectetur.tsx
@@ -12,68 +12,66 @@ import { Footer } from '../../components/Footer/Footer';
 export const CondimentumConsectetur = () => {
     return (
         <>
-            <div className="condimentumConsectetur-background-container">
-                <NavBar />
+            <NavBar />
 
-                <div className='container'>
-                    <div className=''>
-                        <div className='title-condimentum-consectetur-container'>
-                            <h1 className='title-condimentum-consectectur'>
-                                Condimentum consectetur
-                            </h1>
-                        </div>
-
-                        <RowFilter />
+            <div className='container'>
+                <div className=''>
+                    <div className='title-condimentum-consectetur-container'>
+                        <h1 className='title-condimentum-consectectur'>
+                            Condimentum consectetur
+                        </h1>
                     </div>
 
-                    <div className='principal-container'>
+                    <RowFilter />
+                </div>
 
-                        <div className='container-main'>
-                            <div className='sortby-result-row'>
+                <div className='principal-container'>
 
-                                <div className='number-items'>
-                                    <span>Showing 20 - from 125</span>
-                                </div>
+                    <div className='container-main'>
+                        <div className='sortby-result-row'>
 
-                                <div className='sort-filter'>
-                                    <div className='container-dropdown-sort container-sortby'>
-                                        <div className='container-adjust'>
-                                            <img src={FilterSVG} className='filter' />
-                                            <div className='dropdown-span-column' >
-                                                <span className='title-price'>Sort By</span>
-                                                <span className='title-all'>Featured</span>
-                                            </div>
-                                        </div>
-                                        <div className='container-arrow-down'>
-                                            <img src={AccordianArrowSVG} className='accordian-arrow-price' />
+                            <div className='number-items'>
+                                <span>Showing 20 - from 125</span>
+                            </div>
+
+                            <div className='sort-filter'>
+                                <div className='container-dropdown-sort container-sortby'>
+                                    <div className='container-adjust'>
+                                        <img src={FilterSVG} className='filter' />
+                                        <div className='dropdown-span-column' >
+                                            <span className='title-price'>Sort By</span>
+                                            <span className='title-all'>Featured</span>
                                         </div>
                                     </div>
+                                    <div className='container-arrow-down'>
+                                        <img src={AccordianArrowSVG} className='accordian-arrow-price' />
+                                    </div>
                                 </div>
-
                             </div>
 
-                            <div className='container-card'>
-                                <Card />
-                                <Card />
-                                <Card />
-                                <Card />
-                                <Card />
-                                <Card />
-                                <Card />
-                                <Card />
-                                <Card />
-                                <Card />
-                                <Card />
-                                <Card />
-                                <Card />
-                                <Card />
-                                <Card />
-                            </div>
-
-                            <Pagination />
                         </div>
 
+                        <div className='container-card'>
+                            <Card />
+                            <Card />
+                            <Card />
+                            <Card />
+                            <Card />
+                            <Card />
+                            <Card />
+                            <Card />
+                            <Card />
+                            <Card />
+                            <Card />
+                            <Card />
+                            <Card />
+                            <Card />
+                            <Card />
+                        </div>
+
+                        <Pagination />
                     </div>
+
                 </div>
             </div>
 
